be127700b64d88492fd2ae39e59161bc24fc79e2
Patrick Valsecchi <patrick@thus.ch>
2020-02-04 15:54:22 +0100

    Try to fix initial install

--- runtime/docker-entrypoint.sh
+++ runtime/docker-entrypoint.sh
@@ -5,6 +5,6 @@ rm -f ${APACHE_RUN_DIR}/apache2.pid
 mkdir -p /var/www/data
 touch /var/www/data/.ocdata
 envsubst < /config.php.tmpl > /var/www/html/config/config.php
-cd /var/www/html && php occ upgrade
+(cd /var/www/html && php occ upgrade) || true
 
 exec "$@"
