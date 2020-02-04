9c83a46bbbae76d44c744159ff6eb7d3a5a94f18
Patrick Valsecchi <patrick@thus.ch>
2020-02-04 16:42:49 +0100

    Try to fix initial install

--- runtime/docker-entrypoint.sh
+++ runtime/docker-entrypoint.sh
@@ -5,6 +5,12 @@ rm -f ${APACHE_RUN_DIR}/apache2.pid
 mkdir -p /var/www/data
 touch /var/www/data/.ocdata
 envsubst < /config.php.tmpl > /var/www/html/config/config.php
-(cd /var/www/html && php occ upgrade) || true
+
+cd /var/www/html
+if ! php occ upgrade
+then
+  sed -ie "s/'installed' => true/'installed' => false/" /var/www/html/config/config.php
+  php occ maintenance:install --admin-pass=admin
+fi
 
 exec "$@"
